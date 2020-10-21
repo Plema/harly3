6275c868bf6467d4c0b5be28d7b29f6cf43b69f1
natamaz <sidorova87@mail.ru>
2020-10-21 14:47:17 +0300

    UNI_main_3

---
+++ assets/js/main3.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    <!-- слайдер -->
+    $(function() {
+        $('.marquee').marquee({
+            duration: 23000,
+            startVisible: true,
+            duplicated: true
+        });
+    });
+    <!--  слайдер -->
+
+});
+/**
+ * Created by Nafta on 19.10.2020.
+ */
